ae66baa08f64e21894a14814681e03b16ed49b77
nathanfi <nathanfinkle@gmail.com>
2016-04-01 18:03:02 -0400

    Trial

--- Extension.js
+++ Extension.js
@@ -24,46 +24,48 @@
     var lan1 = '';
     var lan2 = '';
     var the_word = '';
-    ext.setupLanguages = function(lang1,lang2) {
-      if (lang1 == 'English') {
-        lan1 = 'en';
-      } else if (lang1 == 'Spanish') {
-        lan1 = 'es';
-      } else if (lang1 == 'Italian') {
-        lan1 = 'it';
-      } else if (lang1 == 'Chinese') {
-        lan1 = 'zh';
-      } else if (lang1 == 'German') {
-        lan1 = 'de';
-      } else if (lang1 == 'Russian') {
-        lan1 = 'ru';
-      } else if (lang1 == 'French') {
-        lan1 = 'fr';
-      }
-      if (lang2 == 'English') {
-        lan2 = 'en';
-      } else if (lang2 == 'Spanish') {
-        lan2 = 'es';
-      } else if (lang2 == 'Italian') {
-        lan2 = 'it';
-      } else if (lang2 == 'Chinese') {
-        lan2 = 'zh';
-      } else if (lang2 == 'German') {
-        lan2 = 'de';
-      } else if (lang2 == 'Russian') {
-        lan2 = 'ru';
-      } else if (lang2 == 'French') {
-        lan2 = 'fr';
-      }
-    };
+    // ext.setupLanguages = function(lang1,lang2) {
+    //   if (lang1 == 'English') {
+    //     lan1 = 'en';
+    //   } else if (lang1 == 'Spanish') {
+    //     lan1 = 'es';
+    //   } else if (lang1 == 'Italian') {
+    //     lan1 = 'it';
+    //   } else if (lang1 == 'Chinese') {
+    //     lan1 = 'zh';
+    //   } else if (lang1 == 'German') {
+    //     lan1 = 'de';
+    //   } else if (lang1 == 'Russian') {
+    //     lan1 = 'ru';
+    //   } else if (lang1 == 'French') {
+    //     lan1 = 'fr';
+    //   }
+    //   if (lang2 == 'English') {
+    //     lan2 = 'en';
+    //   } else if (lang2 == 'Spanish') {
+    //     lan2 = 'es';
+    //   } else if (lang2 == 'Italian') {
+    //     lan2 = 'it';
+    //   } else if (lang2 == 'Chinese') {
+    //     lan2 = 'zh';
+    //   } else if (lang2 == 'German') {
+    //     lan2 = 'de';
+    //   } else if (lang2 == 'Russian') {
+    //     lan2 = 'ru';
+    //   } else if (lang2 == 'French') {
+    //     lan2 = 'fr';
+    //   }
+    // };
     ext.translate = function(word, lang1, lang2, option, callback) {
-      if (option == 'Language'){
-        setupLanguages(lang1,lang2);
-      }
+      // if (option == 'Language'){
+      //   setupLanguages(lang1,lang2);
+      // }
       // var jsonRequest = new XMLHttpRequest();
       // jsonRequest.onreadystatechange = whatIsTheWord;
       // jsonRequest.open('GET', url_beg + "&text=" + word + "&lang=" + lan1 + "-" + lan2);
       // jsonRequest.send();
+      lan1 = 'en';
+      lan2 = 'es';
       getRequest(url_beg + "&text=" + word + "&lang=" + lan1 + "-" + lan2, function(data) {the_word = JSON.parse(data);});
       callback(the_word.languages[0]);
     };
@@ -72,10 +74,10 @@
     //   loadJSON('https://restcountries.eu/rest/v1/alpha/'+countrycode);
     //   lan2 = data.languages[0];
     // };
-    ext.whatIsTheWord = function() {
-      var JSONtext = jsonRequest.responseText;
-        the_word = JSON.parse(JSONtext);
-    };
+    // ext.whatIsTheWord = function() {
+    //   var JSONtext = jsonRequest.responseText;
+    //     the_word = JSON.parse(JSONtext);
+    // };
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
