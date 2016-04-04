f7b6aa99880e109af83a378d36d6a9a8f260080a
nathanfi <nathanfinkle@gmail.com>
2016-04-04 14:44:57 -0400

    ext

--- Translate/Translate.js
+++ Translate/Translate.js
@@ -6,34 +6,33 @@
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
     };
-    var url_beg = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160330T170050Z.604550f9f0ae2dd3.cf0f23a139379f9aa5513f13b7a06eabeb1898ad";
-    var word = 'Hello';
-    // var lang1 = "en";
-    // var lang2 = "es";
-    var lan1 = '';
+    var url_beg = "https://translate.yandex.net/api/v1.5/tr.json/translate";
+    var key = "trnsl.1.1.20160330T170050Z.604550f9f0ae2dd3.cf0f23a139379f9aa5513f13b7a06eabeb1898ad";
+    //var word = 'Hello';
     var lan2 = '';
     var the_word = '';
     var lang = [];
     var lan = [];
     ext.setupLanguages = function() {
-      for (i=0;i<2;i++) {
+      for (i=0;i<=1;i++) {
         switch(lang[i]) {
-          case 'English'   : lan[i] = 'en'; break;
-          case 'Spanish'   : lan[i] = 'es'; break;
-          case 'Chinese'   : lan[i] = 'zh'; break;
-          case 'Italian'   : lan[i] = 'it'; break;
-          case 'German'    : lan[i] = 'de'; break;
-          case 'Russian'   : lan[i] = 'ru'; break;
-          case 'French'    : lan[i] = 'fr'; break;
-          case 'Japanese'  : lan[i] = 'ja'; break;
+          case 'English':    lan[i] = 'en'; break;
+          case 'Spanish':    lan[i] = 'es'; break;
+          case 'Chinese':    lan[i] = 'zh'; break;
+          case 'Italian':    lan[i] = 'it'; break;
+          case 'German':     lan[i] = 'de'; break;
+          case 'Russian':    lan[i] = 'ru'; break;
+          case 'French':     lan[i] = 'fr'; break;
+          case 'Japanese':   lan[i] = 'ja'; break;
           case 'Portuguese': lan[i] = 'pt'; break;
         }
       }
     };
     ext.translate = function(word, language1, language2, callback) {
+      var word_input = word;
       lang[0] = language1;
       lang[1] = language2;
-      setupLanguages();
+      ext.setupLanguages();
       var jsonRequest = new XMLHttpRequest();
       jsonRequest.onreadystatechange = function() {
         if (jsonRequest.readyState === XMLHttpRequest.DONE) {
@@ -42,7 +41,7 @@
             callback(the_word);
         }
       };
-      jsonRequest.open("GET", url_beg + "&text=" + word + "&lang=" + lan[0] + "-" + lan[1]);
+      jsonRequest.open("GET", url_beg + "?key=" + key + "&text=" + word_input + "&lang=" + lan[0] + "-" + lan[1]);
       jsonRequest.send();
     };
     // Block and block menu descriptions
