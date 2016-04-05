dd2b70a9b329afacef9bd257940aecb939e8223c
nathanfi <nathanfinkle@gmail.com>
2016-04-05 10:09:51 -0400

    lowercase

--- Extension.js
+++ Extension.js
@@ -10,37 +10,23 @@
     var the_word = '';
     var word_input = '';
     var languagecode = '';
-    var lattitude = '47.03';
+    var latitude = '47.03';
     var longitude = '10.2';
     var countrycode = '';
-
-    ext.translate = function() {
-      var jsonRequestTranslate = new XMLHttpRequest();
-      jsonRequestTranslate.onreadystatechange = function() {
-        if (jsonRequestTranslate.readyState === XMLHttpRequest.DONE) {
-        var JSONtext = jsonRequestTranslate.responseText;
-            the_word = JSON.parse(JSONtext).text[0];
-            // callback(the_word);
-        }
-      };
-      var url_beg = "https://translate.yandex.net/api/v1.5/tr.json/translate?";
-      var key = "trnsl.1.1.20160330T170050Z.604550f9f0ae2dd3.cf0f23a139379f9aa5513f13b7a06eabeb1898ad";
-      jsonRequestTranslate.open("GET", url_beg + "key=" + key + "&text=" + word_input + "&lang=" + languagecode);
-      jsonRequestTranslate.send();
-    };
 
     ext.getCountry = function() {
       var jsonRequestCountry = new XMLHttpRequest();
       jsonRequestCountry.onreadystatechange = function() {
         if (jsonRequestCountry.readyState === XMLHttpRequest.DONE) {
-        // var JSONtextCountry = jsonRequestCountry.responseText;
-        //     countrycode = String(JSON.parse(JSONtextCountry));
-            countryCode = String(jsonRequestCountry.responseText);
+        var JSONtextCountry = jsonRequestCountry.responseText;
+        var countrycode_uppercase = JSON.parse(JSONtextCountry);
+          countryCode = countrycode_uppercase.toLowerCase();
+            // countryCode = jsonRequestCountry.responseText;
 
         }
       };
       var url_beg = 'http://api.geonames.org/countryCode?';
-      jsonRequestCountry.open("GET", url_beg + 'lat=' + lattitude + '&lng=' + longitude + '&username=nathanfi');
+      jsonRequestCountry.open("GET", url_beg + 'lat=' + latitude + '&lng=' + longitude + '&username=nathanfi');
       jsonRequestCountry.send();
     };
 
@@ -55,6 +41,21 @@
       var url_beg = 'https://restcountries.eu/rest/v1/alpha/';
       jsonRequestLanguage.open("GET", url_beg + countrycode);
       jsonRequestLanguage.send();
+    };
+
+    ext.translate = function() {
+      var jsonRequestTranslate = new XMLHttpRequest();
+      jsonRequestTranslate.onreadystatechange = function() {
+        if (jsonRequestTranslate.readyState === XMLHttpRequest.DONE) {
+        var JSONtext = jsonRequestTranslate.responseText;
+            the_word = JSON.parse(JSONtext).text[0];
+            // callback(the_word);
+        }
+      };
+      var url_beg = "https://translate.yandex.net/api/v1.5/tr.json/translate?";
+      var key = "trnsl.1.1.20160330T170050Z.604550f9f0ae2dd3.cf0f23a139379f9aa5513f13b7a06eabeb1898ad";
+      jsonRequestTranslate.open("GET", url_beg + "key=" + key + "&text=" + word_input + "&lang=" + languagecode);
+      jsonRequestTranslate.send();
     };
 
     ext.execute = function(word, callback) {
