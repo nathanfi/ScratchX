69ed1f419da65e10c49f1e436aca1d65b5bfa90d
nathanfi <nathanfinkle@gmail.com>
2016-04-06 13:24:51 -0400

    Revert to previous

--- CountryInfo/Info.js
+++ CountryInfo/Info.js
@@ -7,48 +7,35 @@
         return {status: 2, msg: 'Ready'};
     };
     var answer = '';
-    var option = '';
-    var howMany = 1;
     var url_option = '?fullText=true';
-    var country = '';
 
     ext.getInfo = function(option_input, country_input, callback) {
-      // if (howMany == 1) {
-      option = option_input;
-      country = country_input;
-      // }
-      // try {
+      var option = option_input;
+      var country = country_input;
       var jsonRequest = new XMLHttpRequest();
       jsonRequest.onreadystatechange = function() {
         if (jsonRequest.readyState === XMLHttpRequest.DONE) {
           var JSONtext = jsonRequest.responseText;
-          if (option == 'Capital') {
-            answer = JSON.parse(JSONtext)[0].capital;
-          } else if (option == 'Region') {
-            answer = JSON.parse(JSONtext)[0].region;
-          } else if (option == 'Subregion') {
-            answer = JSON.parse(JSONtext)[0].subregion;
-          } else if (option == 'Population') {
-            answer = JSON.parse(JSONtext)[0].population;
-          }
-          callback(answer);
-            // answer = '';
-            // jsonRequest = undefined;
-          // }
+          try {
+              if (option == 'Capital') {
+                answer = JSON.parse(JSONtext)[0].capital;
+              } else if (option == 'Region') {
+                answer = JSON.parse(JSONtext)[0].region;
+              } else if (option == 'Subregion') {
+                answer = JSON.parse(JSONtext)[0].subregion;
+              } else if (option == 'Population') {
+                answer = JSON.parse(JSONtext)[0].population;
+              }
+            } catch (error) {
+              answer = 'N/A';
+            } finally {
+              callback(answer);
+            }
         }
         var url_beg = 'https://restcountries.eu/rest/v1/name/';
         jsonRequest.open("GET", url_beg + country + url_option);
         jsonRequest.send();
-
-      }; //catch (error) {
-      //     if (howMany == 1) {
-      //       howMany++;
-      //       url_option = '';
-      //       ext.getInfo();
-      //     } else if (howMany == 2) {
-      //       answer = 'N/A';
-      //     }
-      // }
+      };
     };
     // Block and block menu descriptions
     var descriptor = {
