ad39b2fb32cfdf398326d8fd72c640012aa9946f
nathanfi <nathanfinkle@gmail.com>
2016-04-13 15:11:41 -0400

    back

--- CountryInfo/Info.js
+++ CountryInfo/Info.js
@@ -13,23 +13,29 @@
     var url_beg = 'https://restcountries.eu/rest/v1/name/';
     var didTwoWork;
     var didOneWork;
-    var howManyTimesThrough = 0;
 
-    ext.getInfo = function(option_input, country_input) {
+    ext.getInfo = function(option_input, country_input, callback) {
       var option = option_input;
       var country = country_input;
       var jsonRequest2 = new XMLHttpRequest();
-      jsonRequest2.onreadystatechange = theFunction(option_input, country_input);
+      jsonRequest2.onreadystatechange = function() {
+        if (jsonRequest2.readyState === XMLHttpRequest.DONE) {
+          var JSONtext2 = jsonRequest2.responseText;
+          if (JSON.parse(JSONtext2).status == 404) {
+            didTwoWork = 'no';
+          } else if (option == 'Capital') {
+            answer2 = JSON.parse(JSONtext2)[0].capital;
+          } else if (option == 'Region') {
+            answer2 = JSON.parse(JSONtext2)[0].region;
+          } else if (option == 'Subregion') {
+            answer2 = JSON.parse(JSONtext2)[0].subregion;
+          } else if (option == 'Population') {
+            answer2 = JSON.parse(JSONtext2)[0].population;
+          }
+        }
+      };
       jsonRequest2.open("GET", url_beg + country);
       jsonRequest2.send();
-    };
-    ext.requestFull = function(option_input, country_input, callback) {
-      if (howManyTimesThrough > 0) {
-        ext.getInfo(option_input, country_input);
-        setTimeout(function() {}, 2000);
-      }
-      var option = option_input;
-      var country = country_input;
       var jsonRequest = new XMLHttpRequest();
       jsonRequest.onreadystatechange = function() {
         if (jsonRequest.readyState === XMLHttpRequest.DONE) {
@@ -61,28 +67,11 @@
       jsonRequest.open("GET", url_beg + country + url_option);
       jsonRequest.send();
     };
-    function theFunction(option_input, country_input) {
-      if (jsonRequest2.readyState === XMLHttpRequest.DONE) {
-        var JSONtext2 = jsonRequest2.responseText;
-        if (JSON.parse(JSONtext2).status == 404) {
-          didTwoWork = 'no';
-        } else if (option == 'Capital') {
-          answer2 = JSON.parse(JSONtext2)[0].capital;
-        } else if (option == 'Region') {
-          answer2 = JSON.parse(JSONtext2)[0].region;
-        } else if (option == 'Subregion') {
-          answer2 = JSON.parse(JSONtext2)[0].subregion;
-        } else if (option == 'Population') {
-          answer2 = JSON.parse(JSONtext2)[0].population;
-        }
-        howManyTimesThrough++;
-        ext.requestFull(option_input, country_input);
-      }
-    }
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-          ['R', '%m.option_input of %s', 'requestFull', 'Capital', 'Afghanistan']
+          ['R', '%m.option_input of %s', 'getInfo', 'Capital', 'Afghanistan']
         ],
         menus: {
           option_input: ['Capital', 'Region', 'Sub-Region', 'Population']
