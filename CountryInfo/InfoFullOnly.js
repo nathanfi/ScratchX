818aaa3df4b981decfabf7623371bae6de996303
nathanfi <nathanfinkle@gmail.com>
2016-04-25 20:14:26 -0400

    toString();

--- CountryInfo/InfoFullOnly.js
+++ CountryInfo/InfoFullOnly.js
@@ -31,7 +31,8 @@
               answer1 = JSON.parse(JSONtext1)[0].region;
               didOneWork = 'yes';
             } else if (option == 'Population') {
-              var answer2 = string(JSON.parse(JSONtext1)[0].population);
+              var stuffsucks = JSON.parse(JSONtext1)[0].population;
+              var answer2 = stuffsucks.toString();
               didOneWork = 'yes';
               answer1 = answer2;
               /*var answer3 = answer2.split('');
