e104c50b414cf244e684459a2729d07f93978cad
nathanfi <nathanfinkle@gmail.com>
2016-05-04 13:27:28 -0400

    Change Input name

--- Extension.js
+++ Extension.js
@@ -47,7 +47,7 @@
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-          ['R', 'Display %s from longitude, latitude %s,%s', 'execute', 'Hello', '12.0', '54.0']
+          ['R', '%s from long,lat %s , %s', 'execute', 'Hello', '12.0', '54.0']
         ]
     };
 
