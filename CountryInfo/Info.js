97ac0a9bec504e34bf4fe5fab7f72e04b8e343de
nathanfi <nathanfinkle@gmail.com>
2016-05-11 14:15:01 -0400

    .toString()).split('')

--- CountryInfo/Info.js
+++ CountryInfo/Info.js
@@ -32,7 +32,7 @@
                                     for (i = 0; i < filler.length; i++) { output = output.concat(filler[i]); } break;
               case 'Population Density': pop = ((JSON.parse(fullNameText)[0].population).toString()).split('');
                                     area = ((JSON.parse(fullNameText)[0].area).toString()).split('');
-                                    filler = Math.round((parseInt(pop)/parseInt(area)));
+                                    filler = ((Math.round((parseInt(pop)/parseInt(area)))).toString()).split('');
                                     for (i=filler.length-3; i >0; i=i-3) { filler.splice(i, 0, ','); }
                                     for (i = 0; i < filler.length; i++) { output = output.concat(filler[i]); } break;
             }
