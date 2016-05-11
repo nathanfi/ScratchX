a4e1ae5a6cbebe60811cd5395f1ffdf35ab30003
nathanfi <nathanfinkle@gmail.com>
2016-05-11 12:59:42 -0400

    no filler2

--- CountryInfo/Info.js
+++ CountryInfo/Info.js
@@ -35,7 +35,7 @@
                                     for (i = 0; i < filler.length; i++) { pop = pop.concat(filler[i]); }
                                     filler = ((JSON.parse(fullNameText)[0].area).toString()).split('');
                                     for (i=filler.length-3; i >0; i=i-3) { filler.splice(i, 0, ','); }
-                                    for (i = 0; i < filler.length; i++) { area = area.concat(filler2[i]); }
+                                    for (i = 0; i < filler.length; i++) { area = area.concat(filler[i]); }
                                     /*
                                     filler = round(parseInt(pop)/parseInt(area));
                                     for (i=filler.length-3; i >0; i=i-3) { filler.splice(i, 0, ','); }
@@ -64,7 +64,7 @@
                     case 'Area':        filler = ((JSON.parse(halfNameText)[0].area).toString()).split('');
                                           for (i=filler.length-3; i >0; i=i-3) { filler.splice(i, 0, ','); }
                                           for (i = 0; i < filler.length; i++) { output = output.concat(filler[i]); } break;
-                    case 'Population Density': var pop; var area; filler = ((JSON.parse(halfNameText)[0].population).toString()).split('');
+                    case 'Population Density': filler = ((JSON.parse(halfNameText)[0].population).toString()).split('');
                                           for (i=filler.length-3; i >0; i=i-3) { filler.splice(i, 0, ','); }
                                           for (i = 0; i < filler.length; i++) { pop = pop.concat(filler[i]); }
                                           filler = ((JSON.parse(halfNameText)[0].area).toString()).split('');
